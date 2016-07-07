25c66a326238d667920622d8cf97f6cf06af7b97
Alan C <alanc@kidney.org>
2016-07-06 20:25:18 -0400

    Initial Repo for Redesign of Big Ask and Big Give

---
+++ bootstrap-sass-3.3.4/config.rb
@@ -0,0 +1,3 @@
+add_import_path 'bower_components/bootstrap-sass-official/assets/stylesheets'
+add_import_path 'bower_components/breakpoint-sass/stylesheets'
+add_import_path 'bower_components/singularity/stylesheets'
