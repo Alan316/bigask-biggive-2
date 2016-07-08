e495d18e596389d03ef75656b4eccf7ef9720347
Alan C <alanc@kidney.org>
2016-07-08 15:39:51 -0400

    Added Bootstrap and code for splashpage

--- bootstrap-sass-3.3.4/config.rb
+++
@@ -1,3 +0,0 @@
-add_import_path 'bower_components/bootstrap-sass-official/assets/stylesheets'
-add_import_path 'bower_components/breakpoint-sass/stylesheets'
-add_import_path 'bower_components/singularity/stylesheets'
